e0304737f7036e08906f679eca8b278272fe1d08
Chris Phillipson <cphillipson@pivotal.io>
2023-02-05 18:01:58 -0800

    Upgrade to Java 17 and Spring Boot 3

--- src/main/java/com/fns/primes/PrimesController.java
+++ src/main/java/com/fns/primes/PrimesController.java
@@ -2,7 +2,7 @@ package com.fns.primes;
 
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
